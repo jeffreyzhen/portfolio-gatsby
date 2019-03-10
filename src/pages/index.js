b3009e940d4d831eb4fa91fee83928d80bd215c5
Jeffrey Zhen <jzhen91@gmail.com>
2019-03-10 17:45:46 -0400

    Add keywords

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,28 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
-import Image from '../components/image';
 import SEO from '../components/seo';
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <SEO
+      title="Jeffrey Zhen - Front-end Developer"
+      keywords={[
+        `portfolio`,
+        `developer`,
+        `react`,
+        `front-end`,
+        `javascript`,
+        `boston`,
+        `gatsby`,
+        `react-native`,
+        `node`,
+      ]}
+    />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div>
-      <Image />
-    </div>
+
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 );
