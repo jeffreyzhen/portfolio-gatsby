2100e5c07d54fe2b01c946656a5df424605e63f5
Jeffrey Zhen <jzhen91@gmail.com>
2019-03-17 17:19:06 -0400

    add device queries

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,13 +3,17 @@ import PropTypes from 'prop-types';
 import { StaticQuery, graphql } from 'gatsby';
 import styled from '@emotion/styled';
 import GlobalStyles from './global';
+import device from './device';
 import Header from './header';
 
 const Main = styled.main`
   /* padding-top: 34px; */
   width: 100%;
   margin: 0px auto;
-  padding: 0px 150px;
+  padding: 0 25px;
+  ${device.phoneXL`padding: 0 50px`}
+  ${device.tablet`padding: 0 100px`}
+  ${device.desktop`padding: 0 150px`}
 `;
 
 const Layout = ({ children }) => (
